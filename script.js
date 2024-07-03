f689f31459275295e0d0b676564d59cbd663ca7e
Daniel Ramos <90581303+drammistein@users.noreply.github.com>
2024-07-03 14:37:33 -0300

    Alteração no layout do site.

--- script.js
+++ script.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
         $('#' + sectionId).removeClass('d-none');
         $('.navbar-collapse').collapse('hide'); // Fechar o menu ao clicar em um item
     });
+
+    // Mostrar todas as seções em telas menores
+    function handleResize() {
+        if ($(window).width() < 768) {
+            $('.section').removeClass('d-none');
+        } else {
+            $('.section').not(':first').addClass('d-none');
+        }
+    }
+
+    $(window).resize(handleResize);
+    handleResize();
 });
